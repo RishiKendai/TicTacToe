9055de23500767ea258e90a0b92ee587e74e1f86
RishiKendai <kendairishikesh5@gmail.com>
2022-05-01 22:17:57 +0530

    simple tic tac toe using vanilla js

---
+++ app.js
@@ -0,0 +1,143 @@
+// CONSTANTS
+const X_CLASS = 'x';
+const O_CLASS = 'o';
+let circleTurn;
+let checkCombo = [];
+const WINNING_COMBO = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+const verticalCombo = [
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+];
+const horizontalCombo = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+];
+const leftDiagonal = [[0, 4, 8]];
+const rightDiagonal = [[2, 4, 6]];
+
+//  SELECTORS
+const cellElements = document.querySelectorAll('[data-cell]');
+const strikes = document.querySelectorAll('.strike');
+const board = document.querySelector('[board]');
+const winningMessgeTab = document.querySelector('.winningMessgeTab');
+const winningMessageText = document.querySelector('.winningMessageText');
+const restart = document.querySelector('.restart');
+
+// CHECK DIRECTION  AND STRIKE LINE
+function checkSide() {
+  const directionObj = [
+    { name: 'vertical', check: verticalCombo, isTrue: false },
+    { name: 'horizontal', check: horizontalCombo, isTrue: false },
+    { name: 'diagonal-left', check: leftDiagonal, isTrue: false },
+    { name: 'diagonal-right', check: rightDiagonal, isTrue: false },
+  ];
+  directionObj.forEach((array) => {
+    array.isTrue = array.check.some((elements) => {
+      return elements.every((index) => {
+        return checkCombo.includes(index);
+      });
+    });
+    array.isTrue &&
+      checkCombo.forEach((ele) => strikes[ele].classList.add(array.name));
+  });
+}
+
+// ------- INITIAL FUNCTION -------
+startGame();
+
+// EVENT LISTENER
+restart.addEventListener('click', startGame);
+
+// ------ APP STARTS HERE -------
+function startGame() {
+  let circleTurn = false;
+  cellElements.forEach((cell, index) => {
+    cell.className = 'cell'; // REMOVE ALL THE CLASS NAME AND SET INITIAL CLASS NAME
+    strikes[index].className = 'strike'; // REMOVE ALL THE CLASS NAME AND SET INITIAL CLASS NAME
+    winningMessgeTab.classList.remove('show'); // REMOVE CLASS NAME FOR WINNING TAB
+    cell.removeEventListener('click', handleClick); // REMOVE ALL EVENT LISTENERS
+    cell.addEventListener('click', handleClick, { once: true }); // LISTEN TO EACH CELL ONLY ONCE
+  });
+  boardHoverClass();
+}
+
+//  FUNCTION WHEN CLICK EVENT TRIGGERED
+function handleClick(e) {
+  const cell = e.target;
+  const currentClass = circleTurn ? O_CLASS : X_CLASS;
+  placeMark(cell, currentClass); // MARK 'X' OR 'O' ON THE CELL
+  if (checkWin(currentClass)) {
+    // CHECK WIN
+    checkSide();
+    endGame(false);
+  } else if (isDraw()) {
+    // CHECK DRAW
+    endGame(true);
+  }
+  swapTurns(); // ELSE SWAP THE TURN
+  boardHoverClass();
+}
+
+// FUNCTION TO PLACE THE 'X' OR 'O' ON THE CELL
+function placeMark(cell, currentClass) {
+  cell.classList.add(currentClass);
+}
+
+// CHECK DRAW
+function isDraw() {
+  return [...cellElements].every((cell) => {
+    return cell.classList.contains(X_CLASS) || cell.classList.contains(O_CLASS);
+  });
+}
+
+// CHECK WIN
+function checkWin(currentClass) {
+  return WINNING_COMBO.some((combo) => {
+    let derivedCombo = combo.every((index) => {
+      if (cellElements[index].classList.contains(currentClass))
+        return cellElements[index].classList.contains(currentClass);
+    });
+    if (derivedCombo) {
+      checkCombo = combo;
+    }
+    return derivedCombo;
+  });
+}
+
+// FUNCTION TO DISPLAY THE RESULT TAB
+function endGame(draw) {
+  if (draw) {
+    winningMessageText.textContent = 'Draw';
+  } else {
+    winningMessageText.textContent = `${circleTurn ? "O's" : "X's"} Win`;
+  }
+  setTimeout(() => {
+    winningMessgeTab.classList.add('show');
+  }, 1200);
+}
+
+// SWAP TURN
+function swapTurns() {
+  circleTurn = !circleTurn;
+}
+
+// FUNCTION TO HOVER 'X' OR 'O' ON CELL
+function boardHoverClass() {
+  board.className = 'board'; // REMOVE OTHER CLASSNAME AND SET DEFAULT
+  if (circleTurn) {
+    board.classList.add(O_CLASS);
+  } else {
+    board.classList.add(X_CLASS);
+  }
+}
